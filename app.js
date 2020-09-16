f53a128d55de5571f2fe176ab2ca6729bb3e87ec
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-09-17 01:21:32 +0600

    senFile to sendFile

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/api/articles", require("./routes/articles.routes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
-    res.senFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
